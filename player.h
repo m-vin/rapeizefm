2f941bdf8cc87765bb9972cf3456f9de36fef975
m-vin <mvitr.araceli@gmail.com>
2017-11-18 15:09:41 -0200

    Update player.h

--- player.h
+++ player.h
@@ -2,6 +2,7 @@
 #define PLAYER_H_
 
 int destroy();
+void swap_tool_button(GtkWidget*, GtkWidget*);
 void play_music();
 void pause_music();
 void check_music();
@@ -11,6 +12,7 @@ const char* get_artist();
 const char* get_album();
 const char* get_title();
 void rapeizefm_audio_init();
+void set_audio();
 static void about();
 
 #endif
